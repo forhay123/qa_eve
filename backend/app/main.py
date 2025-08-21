56358f51e6ef01a3c80f81d76ec76b1ba29e7984
forhay123 <hatbee555@example.com>
2025-08-21 09:10:02 +0100

    Fix missing Request import for catch-all route

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi.security import OAuth2PasswordBearer
 from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel, OAuthFlowPassword
 from fastapi.openapi.utils import get_openapi
 from fastapi.security import SecurityScopes
-from fastapi import Security
+from fastapi import Security, Request 
 
 from fastapi import FastAPI, UploadFile, File, Depends, HTTPException, status, Body, Query
 from fastapi.middleware.cors import CORSMiddleware
